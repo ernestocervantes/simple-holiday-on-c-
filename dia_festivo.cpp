3b1db091fd96fe2fdea0335e3944cad98b1eaf7a
Jorge Ernesto Cervantes <ernestocervantes@users.noreply.github.com>
2019-08-19 23:41:25 -0500

    Add files via upload
    obtener si un día es festivo o no, usando programación básica en C++ declarando datos tipo booleano aplicando sentencias if

---
+++ dia_festivo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	bool festivo;
+	int dia = 25;
+	
+	if (dia == 25)
+	{
+		festivo = true;
+	}
+	else
+	{
+		festivo == false;
+	}
+	
+	if (festivo)
+	{
+		cout <<"Festivo\n";
+	}
+	else
+	{
+		cout <<"No festivo\n";
+	}
+	
+	return 0;
+}
+
